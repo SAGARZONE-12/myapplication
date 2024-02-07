7f918829d5dbb33938dc0d73d630684450f998b8
Sagar Kumar Roy <sagarkumarroy2@gmail.com>
2024-02-07 21:13:19 +0530

    Created New Ajax CRUD

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,83 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Student;
+use Illuminate\Http\Request;
+class StudentController extends Controller
+{
+    function student_add(Request $request){
+        $insert = [
+            'name' => $request->name,
+            'email'=> $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+        ];
+        $add = Student::create($insert);
+        if($add){
+            $response = [
+                'status'=>'ok',
+                'success'=>true,
+                'message'=>'Record created succesfully!'
+            ];
+            return $response;
+        }else{
+            $response = [
+                'status'=>'ok',
+                'success'=>false,
+                'message'=>'Record created failed!'
+            ];
+            return $response;
+        }
+    } 
+
+    function student_view(Request $request){
+        return Student::find($request->id);
+    } 
+
+    function student_delete(Request $request){
+        $delete =  Student::destroy($request->id);
+        if($delete){
+            $response = [
+                'status'=>'ok',
+                'success'=>true,
+                'message'=>'Record deleted succesfully!'
+            ];
+            return $response;
+        }else{
+            $response = [
+                'status'=>'ok',
+                'success'=>false,
+                'message'=>'Record deleted failed!'
+            ];
+            return $response;
+        }
+    } 
+
+    function student_edit(Request $request){
+        $update = [
+            'name' => $request->name,
+            'email'=> $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+        ];
+        $edit = Student::where('id', $request->student_id)->update($update);
+        if($edit){
+            $response = [
+                'status'=>'ok',
+                'success'=>true,
+                'message'=>'Record updated succesfully!'
+            ];
+            return $response;
+        }else{
+            $response = [
+                'status'=>'ok',
+                'success'=>false,
+                'message'=>'Record updated failed!'
+            ];
+            return $response;
+        }
+    } 
+
+    function student_list(){
+        return Student::all();
+    }
+} 
